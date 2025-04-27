44d090e61b68cb53dacc1edfffb96d17533cc7c0
tkstan <77407531+tkstan@users.noreply.github.com>
2025-04-26 21:46:37 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MergeFilesApp from './index'; // Ton ancien index.js est ton MergeFilesApp
+
+function App() {
+  return <MergeFilesApp />;
+}
+
+export default App;
